1f7522a2f08394be33b8e1c02f0818752b1d37a1
asude <asuderuken63@gmail.com>
2025-03-22 14:17:26 +0300

    hesap makinesi

---
+++ main.js
@@ -0,0 +1,150 @@
+const buttons = document.querySelectorAll(".number"); 
+const display = document.querySelector(".result");
+const iconDelete = document.querySelector("#icon");
+
+buttons.forEach(button => {
+    button.addEventListener("mousedown", () => {
+        button.style.backgroundColor = "rgb(229, 227, 227)";
+    });
+    button.addEventListener("mouseup", () => {
+        button.style.backgroundColor = "rgb(247, 247, 248)";
+    });
+});
+
+
+let arrays = [];
+buttons.forEach(element => {
+    element.addEventListener("click", () => {
+        let icerik = element.textContent;
+
+        arrays.push(icerik);
+        console.log(arrays);
+        display.textContent = arrays.join("");
+
+        sil(icerik)
+
+        if (icerik === "=") {
+            hesapla();
+        }
+    });
+});
+
+function hesapla() {
+    let islem = arrays.join("").slice(0, -1);
+    while (islem.includes("(")) {
+        let parantezIci = islem.match(/\([^\(\)]+\)/);
+        if (parantezIci) {
+            let parantezliIslem = parantezIci[0].slice(1, -1)
+            let sonuc = islemHesapla(parantezliIslem)
+            islem = islem.replace(parantezIci[0], sonuc) 
+        }
+    }
+    
+    let sayilar = islem.split(/([-+√×/^])/g);
+
+    for (let i = 0; i < sayilar.length; i++) {
+        if (sayilar[i] === "√") {
+            let sonuc = Math.sqrt(parseFloat(sayilar[i + 1]));
+            sayilar.splice(i - 1 , 3, sonuc)
+            i--
+        }
+    }
+
+    for(let i=0; i<sayilar.length; i++){
+        if(sayilar[i]==="^"){
+            let a=sayilar[i-1]
+            let b=sayilar[i+1]
+            let sonuc= a**b
+            sayilar.splice(i-1, 3, sonuc)
+            i--
+        }
+    }
+
+    for (let i = 0; i < sayilar.length; i++) {
+        if (sayilar[i] === "×" || sayilar[i] === "/") {
+            let a = parseFloat(sayilar[i - 1])
+            let b = parseFloat(sayilar[i + 1])
+
+            let sonuc;
+            if (sayilar[i] === "×") {
+                sonuc = a * b
+            } else {
+                sonuc = a / b
+            }
+            sayilar.splice(i - 1, 3, sonuc)
+            i--
+        }
+    }
+
+    let toplam = parseFloat(sayilar[0]);
+    for (let i = 1; i < sayilar.length; i += 2) {
+        let operator = sayilar[i];
+        let sayi = parseFloat(sayilar[i + 1]);
+
+        if (operator === "+") {
+            toplam += sayi;
+        } else if (operator === "-") {
+            toplam -= sayi;
+        }
+    }
+    display.textContent = toplam;
+    arrays = [toplam];
+}
+
+function islemHesapla(islem) {
+    let sayilar = islem.split(/([-+√×/])/g)
+
+    for (let i = 0; i < sayilar.length; i++) {
+        if (sayilar[i] === "√") {
+            let sonuc = Math.sqrt(parseFloat(sayilar[i + 1]));
+            sayilar.splice(i - 1, 3, sonuc);
+            i--
+        }
+    }
+
+    for (let i = 0; i < sayilar.length; i++) {
+        if (sayilar[i] === "×" || sayilar[i] === "/") {
+            let a = parseFloat(sayilar[i - 1]);
+            let b = parseFloat(sayilar[i + 1]);
+
+            let sonuc;
+            if (sayilar[i] === "×") {
+                sonuc = a * b;
+            } else {
+                sonuc = a / b;
+            }
+            sayilar.splice(i - 1, 3, sonuc);
+            i--
+        }
+    }
+
+    let toplam = parseFloat(sayilar[0]);
+    for (let i = 1; i < sayilar.length; i += 2) {
+        let operator = sayilar[i];
+        let sayi = parseFloat(sayilar[i + 1]);
+
+        if (operator === "+") {
+            toplam += sayi;
+        } else if (operator === "-") {
+            toplam -= sayi;
+        }
+    }
+    return toplam
+}
+
+function sil(icerik) {
+    if (icerik === "AC") {
+        display.textContent = "";
+        arrays = [];
+    }
+}
+
+iconDelete.addEventListener('click', () => {
+    display.textContent = arrays.join("").slice(0, -1);
+    arrays = [display.textContent];
+});
+
+
+
+
+
